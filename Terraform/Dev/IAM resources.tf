a17e81fdf94c95ebf77b64dc2d53a486a64ba130
guy yefet <guy.yefet@gmail.com>
2023-07-07 09:14:21 +0300

    editing subjects

--- Terraform/Dev/IAM resources.tf
+++ Terraform/Dev/IAM resources.tf
@@ -1,5 +1,6 @@
 module "iam_github_oidc_provider" {
-  source    = "../.terraform/modules/iam_github_oidc_provider"
+  source    = "../.terraform/modules/iam_github_oidc_provider/modules/iam-github-oidc-provider"
+
 
 #   client_id_list = ["https://github.com/Guyyefet/infrastructure-pipeline.git"]
 
@@ -9,11 +10,12 @@ module "iam_github_oidc_provider" {
 }
 
 module "iam_github_oidc_role" {
-  source    = "../.terraform/modules/iam_github_oidc_role"
+  source    = "../.terraform/modules/iam_github_oidc_role/modules/iam-github-oidc-role"
 
-  name = "terraform plan role"
-  # This should be updated to suit your organization, repository, references/branches, etc.
-  subjects = ["terraform-aws-modules/terraform-aws-iam:*"]
+  name = "terraform-plan-role"
+  
+  subjects = ["terraform-aws-modules/terraform-aws-iam:*",
+              "Guyyefet/infrastructure-pipeline:*"]
 
   policies = {
     S3ReadOnly = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
