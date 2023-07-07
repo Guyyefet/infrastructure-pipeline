3551fbb0f98a56c05c996447d5875950761b1724
guy yefet <guy.yefet@gmail.com>
2023-07-07 09:15:33 +0300

    adding tls provider and IAM modules

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,8 +14,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "tls" {
+  proxy {
+    url = "https://corporate.proxy.service"
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
+}
+
+module "iam_github_oidc_provider" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = " 5.27.0"
+}
+
+module "iam_github_oidc_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+  version = " 5.27.0"
 }
 
