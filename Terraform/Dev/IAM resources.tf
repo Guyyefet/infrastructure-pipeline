7bf15f972a13dc78790799b46df42769df1916d1
guy yefet <guy.yefet@gmail.com>
2023-07-09 22:00:58 +0300

    new policies creation and attach to a role

--- Terraform/Dev/IAM resources.tf
+++ Terraform/Dev/IAM resources.tf
@@ -1,4 +1,4 @@
-module "aws_iam_policy" {
+module "store-terraform-state-file-in-bucket" {
   source    = "../.terraform/modules/iam_github_oidc_provider/modules/iam-policy"
 
   name = "store-terraform-state-file-in-bucket"
@@ -29,7 +29,66 @@ module "aws_iam_policy" {
 EOF
 }
 
-module "iam_github_oidc_provider" {
+module "dynamoDB-state-locks" {
+  source    = "../.terraform/modules/iam_github_oidc_provider/modules/iam-policy"
+
+  name = "dynamoDB-state-locks"
+
+   policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:DescribeTable",
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+                "dynamodb:DeleteItem"
+            ],
+            "Resource": "arn:aws:dynamodb:*:*:table/terraform-dev-env-locks"
+        }
+    ]
+}
+EOF
+}
+
+module "dev-env-vpc-premisions" {
+  source    = "../.terraform/modules/iam_github_oidc_provider/modules/iam-policy"
+
+  name = "dev-env-vpc-premisions"
+
+   policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AssociateRouteTable",
+                "ec2:AssociateSubnetCidrBlock",
+                "ec2:AssociateVpcCidrBlock",
+                "ec2:CreateRoute",
+                "ec2:CreateRouteTable",
+                "ec2:CreateSecurityGroup",
+                "ec2:CreateSubnet",
+                "ec2:CreateTags",
+                "ec2:CreateVpc",
+                "ec2:DeleteRoute",
+                "ec2:DeleteRouteTable",
+                "ec2:DeleteSecurityGroup",
+                "ec2:DeleteSubnet",
+                "ec2:DeleteTags",
+                "ec2:DeleteVpc"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+module "iam_github_oidc_provider_for_terraform_plan_role" {
   source    = "../.terraform/modules/iam_github_oidc_provider/modules/iam-github-oidc-provider"
 
 
@@ -40,7 +99,7 @@ module "iam_github_oidc_provider" {
   }
 }
 
-module "iam_github_oidc_role" {
+module "terraform-plan-role" {
   source    = "../.terraform/modules/iam_github_oidc_role/modules/iam-github-oidc-role"
 
   name = "terraform-plan-role"
@@ -49,7 +108,10 @@ module "iam_github_oidc_role" {
               "Guyyefet/infrastructure-pipeline:*"]
 
   policies = {
-    store-terraform-state-file-in-bucket = "arn:aws:iam::182021176759:policy/store-terraform-state-file-in-bucket"
+    store-terraform-state-file-in-bucket = module.store-terraform-state-file-in-bucket.arn,
+    EC2_FULL_ACCESS = "arn:aws:iam::182021176759:policy/EC2_FULL_ACCESS",
+    AmazonVPCFullAccess = module.dev-env-vpc-premisions.arn
+    dynamoDB-state-locks = module.dynamoDB-state-locks.arn
   }
 
   tags = {
