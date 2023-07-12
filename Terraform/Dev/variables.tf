0b20e34f9affdc5a374ae11967841af470195f0f
guy yefet <guy.yefet@gmail.com>
2023-07-12 03:04:33 +0300

    new vars added

--- Terraform/Dev/variables.tf
+++ Terraform/Dev/variables.tf
@@ -1,39 +1,39 @@
 variable "region" {
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
   description = "aws region"
 }
 
 variable "environment_name" {
-  type = string
+  type        = string
   description = "the environment name for this resource"
 }
 
 variable "vpc_cidr_block" {
-  type = string
+  type        = string
   description = "vpc_cidr_block"
 }
 
 variable "availability_zones" {
-  type = list(string)
+  type        = list(string)
   description = "value"
 }
 
 variable "public_subnets" {
-  type = list(string)
+  type        = list(string)
   description = "the public subnet in the vpc"
 }
 
 variable "private_subnets" {
-  type = list(string)
+  type        = list(string)
   description = "the private subnet in the vpc"
 }
 
-# variable "instance_type" {
-#   type = string
-#   default = "t2.micro"
-#   description = "instance type"
-# }
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+  description = "instance type"
+}
 
 # variable "local_storage" {
 #   type = string
