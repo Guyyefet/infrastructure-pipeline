5688c412d0b0d09615e30e61ae796bc7f036cf19
guy yefet <guy.yefet@gmail.com>
2023-07-14 00:04:51 +0300

    omiting unnecerry configs

--- Terraform/Dev/backend-config.tf
+++ Terraform/Dev/backend-config.tf
@@ -1,6 +1,6 @@
-module "backend" {
-  source            = "../My-modules/s3-backend"
-  bucket_name       = "terraform-dev-env-state"
-  dynamodb_table    = "terraform-dev-env-locks"
-  bucket_versioning = "Enabled"
-}
+# module "backend" {
+#   source            = "../My-modules/s3-backend"
+#   bucket_name       = "terraform-dev-env-state"
+#   dynamodb_table    = "terraform-dev-env-locks"
+#   bucket_versioning = "Enabled"
+# }
--- Terraform/Dev/main.tf
+++ Terraform/Dev/main.tf
@@ -1,15 +1,15 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-dev-env-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-dev-env-locks"
-    encrypt        = true
-    # }
-    #   backend "local" {
-    #   path = "terraform.tfstate"
-  }
-}
+# terraform {
+#   # backend "s3" {
+#   #   bucket         = "terraform-dev-env-state"
+#   #   key            = "global/s3/terraform.tfstate"
+#   #   region         = "us-east-1"
+#   #   dynamodb_table = "terraform-dev-env-locks"
+#   #   encrypt        = true
+#     # }
+#       backend "local" {
+#       path = "terraform.tfstate"
+#   }
+# }
 
 
 
