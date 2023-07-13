fbe3f304b24b95c68564df5bd490f3b6458f6828
guy yefet <guy.yefet@gmail.com>
2023-07-14 00:00:29 +0300

    removing deleted files

--- Terraform/My-modules/s3-backend/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_id" {
-  description = "The name of the bucket."
-  value       =  try(aws_s3_bucket.this[0].id, null)
-}
-
-output "s3_bucket_arn" {
-  description = ""
-  value = try(aws_s3_bucket.this[0].arn, null)
-}
