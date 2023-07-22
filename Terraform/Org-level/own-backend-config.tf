12fcee862ac2f1d085b4a9e3678a9aeb9df00cd9
guy yefet <guy.yefet@gmail.com>
2023-07-23 02:14:51 +0300

    updating policy_principal_identifiers for backends

--- Terraform/Org-level/own-backend-config.tf
+++ Terraform/Org-level/own-backend-config.tf
@@ -3,4 +3,11 @@ module "org_level_backend" {
   bucket_name       = "terraform-org-level-state"
   dynamodb_table    = "terraform-org-level-locks"
   bucket_versioning = "Enabled"
+  policy_principal_identifiers = [
+    "${data.aws_caller_identity.current.account_id}",
+    "${module.terraform-plan-role.arn}",
+    "${module.infrastructre-group.group_arn}"
+    ]
 }
+
+data "aws_caller_identity" "current" {}
